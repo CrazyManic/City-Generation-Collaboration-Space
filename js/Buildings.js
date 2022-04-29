357a410616f3b6ed795ec44aed22a7cefc6fd6dd
yuzusiooooo <um60ac20@gmail.com>
2022-04-29 17:37:24 +1000

    new building class, will check which building to generate based on the urbanness rate

--- js/Buildings.js
+++ js/Buildings.js
@@ -1,35 +1,67 @@
-var urbaness = 0;
-var bldg_urban_height = 200;
-var bldg_suburban_height = 30;
+var building_baseheight = [30, 100];
 
-// make a building at set position
-function makenew_building(position, uv)
-{
-    size_modifier = get_building_sizemodifier(uv);
-    var bldg_mat = new THREE.MeshPhongMaterial();
-    var bldg_geom = new THREE.BoxGeometry(30, bldg_urban_height + size_modifier[1], 30);
-    var new_bldg = new THREE.Mesh(bldg_geom, bldg_mat);
-    new_bldg.position = position;
-    scene.add(new_bldg);
+class Building{
+        constructor(urbanness, create_position)
+        {
+            console.log("Create new building at "+create_position.x+" "+create_position.y+" "+create_position.z);
+            console.log("Urbanness is "+urbanness);
+
+            // 1: using the urbanness, determine which building to generate
+            // lower to 0 = more chance of a house
+            var threshold_highrise = 100 - urbanness;
+            var buildingtype_rand = Math.random()*100;
+            console.log(buildingtype_rand);
+            if (buildingtype_rand > threshold_highrise)
+            {
+                console.log("Generate highrise");
+            }
+            else{ console.log("Generate house"); }
+
+            // 2: using the urbanness, change the building size
+            // lower to 0 = smaller building
+
+            // set building material and geometry
+            var bldg_mat = new THREE.MeshPhongMaterial();
+            var bldg_geom = new THREE.BoxGeometry(15, 30, 15);            
+            this.buildingMesh = new THREE.Mesh(bldg_geom, bldg_mat);
+
+            // set building position to the given coordinates
+            this.buildingMesh.position.x = create_position.x;
+            this.buildingMesh.position.y = create_position.y;
+            this.buildingMesh.position.z = create_position.z;
+
+            // add building mesh to the scene
+            scene.add(this.buildingMesh);
+
+
+        }
 }
+    // // use the urbanness to determine which building to create
+    // var buildingtype = "highrise";
+    // switch(buildingtype)
+    // {
+    //     // create building geometry/mesh parameters 
+    //     case "house":
+    //         buildingtype = "house";
+    //         var newbldg_height = building_baseheight[0];
+    //         break;
+    //     case "highrise":
+    //         buildingtype = "highrise";
+    //         var newbldg_height = building_baseheight[1];
+    //         break;
+    // }
+    // // add modifiers for randomness between buildings
+    // var modifiers = new Array();
+    // // pass building geometry/mesh parameters here
+    // makenew_building(buildingtype, create_position, newbldg_height);
 
-// use the current user-set urbanness var to make building size modifiers
-function get_building_sizemodifier(uv)
-{
-    // the higher the urbanness is, the taller the building is
-    // ie bigger size modifiers
-    var sidemod_array = new Array(3);
-    for (i=0;i<3;i++)
-    {
-        sidemod_array[i] = uv+getRandomInt(0, 30);
-    }
-    console.log(sidemod_array);
-    return sidemod_array;
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-  
+    // function makenew_building(buildingtype, create_position, bldg_height)
+    // {
+    //     console.log("Create new "+buildingtype+" at "+create_position.x);
+    //     var bldg_mat = new THREE.MeshPhongMaterial();
+    //     var bldg_boxsize = new THREE.Vector3(15, bldg_height, 15);
+    //     var bldg_geom = new THREE.BoxGeometry(bldg_boxsize.x, bldg_boxsize.y, bldg_boxsize.z);
+    //     var new_bldg = new THREE.Mesh(bldg_geom, bldg_mat);
+    //     new_bldg.position = create_position;
+    //     scene.add(new_bldg);
+    // }
