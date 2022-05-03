0ec2b0e1ed33c7a7650efadf7e590f8dabbc599f
JohnCollns <50039786+JohnCollns@users.noreply.github.com>
2022-05-03 11:26:40 +1000

    Refined OOP of Buildings, added functionality
    Buildings now use a Generate() function to do most of their construction, because this can be modified by descendent classes to allow for a great deal of customisation.
    Buildings now take in a colour as a parameter, not sure if I think this is good or should be done internally to the constructor but works thus far.
    Pros: We can set these colours based on the HUD
    Cons: Not internal to the class
    Buildings also have delete functions.

--- js/Buildings.js
+++ js/Buildings.js
@@ -1,40 +1,72 @@
+// 0 - house, 1 - highrise
 var building_baseheight = [30, 100];
+const building_height_randomness = [1.2, 1.7];
+const blockLength = gridSize - streetWidth;
 
 class Building{
-        constructor(urbanness, create_position)
+    constructor(urbanness, create_position, pColor1, pColor2, pColor3)
+    {
+        console.log("Create new building at "+create_position.x+" "+create_position.y+" "+create_position.z);
+        
+        this.height;
+        this.buildingType = 0;
+        this.bldg_geom;
+        this.bldg_mat;
+        // In OOP we put this in a different function, so that it can have a custom definition
+        // for descendant classes, but all other functionality is common to all. 
+        this.Generate(urbanness, pColor1, pColor2, pColor3);
+        
+              
+        this.buildingMesh = new THREE.Mesh(this.bldg_geom, this.bldg_mat);
+
+        // set building position to the given coordinates
+        this.buildingMesh.position.x = create_position.x;
+        this.buildingMesh.position.z = create_position.z;
+        this.buildingMesh.position.y = create_position.y + this.height / 2;
+
+        // add building mesh to the scene
+        scene.add(this.buildingMesh);
+    }
+
+    Generate(urbanness, pColor1, pColor2, pColor3){ // Overwrite this function in descendent classes to implement different building types. 
+        console.log("Urbanness is "+urbanness);
+        // 1: using the urbanness, determine which building to generate
+        // lower to 0 = more chance of a house
+        var threshold_highrise = 100 - urbanness;
+        var buildingtype_rand = Math.random()*100;
+        console.log(buildingtype_rand);
+        // 2: using the urbanness, change the building size
+        // lower to 0 = smaller building
+        if (buildingtype_rand > threshold_highrise)
         {
-            console.log("Create new building at "+create_position.x+" "+create_position.y+" "+create_position.z);
-            console.log("Urbanness is "+urbanness);
-
-            // 1: using the urbanness, determine which building to generate
-            // lower to 0 = more chance of a house
-            var threshold_highrise = 100 - urbanness;
-            var buildingtype_rand = Math.random()*100;
-            console.log(buildingtype_rand);
-            if (buildingtype_rand > threshold_highrise)
-            {
-                console.log("Generate highrise");
-            }
-            else{ console.log("Generate house"); }
-
-            // 2: using the urbanness, change the building size
-            // lower to 0 = smaller building
-
-            // set building material and geometry
-            var bldg_mat = new THREE.MeshPhongMaterial();
-            var bldg_geom = new THREE.BoxGeometry(15, 30, 15);            
-            this.buildingMesh = new THREE.Mesh(bldg_geom, bldg_mat);
-
-            // set building position to the given coordinates
-            this.buildingMesh.position.x = create_position.x;
-            this.buildingMesh.position.y = create_position.y;
-            this.buildingMesh.position.z = create_position.z;
-
-            // add building mesh to the scene
-            scene.add(this.buildingMesh);
-
-
+            console.log("Generate highrise");
+            this.buildingType = 1; // highrise
         }
+        else{ 
+            console.log("Generate house"); 
+            this.buildingType = 0;
+        }
+        
+        this.height = building_baseheight[this.buildingType];// * building_height_randomness[this.buildingType] * Math.random()
+        // would be good to add some random variation
+
+        this.bldg_geom = new THREE.BoxGeometry(blockLength, this.height, blockLength);    
+        this.bldg_mat = new THREE.MeshPhongMaterial();  
+        this.bldg_mat.color = pColor1;
+    }
+
+    Destroy(){
+        scene.remove(this.buildingMesh);
+        this.DestroyExtras();
+        delete this.buildingMesh;
+        delete this.height;
+        delete this.buildingType;
+        delete this;
+    }
+
+    DestroyExtras(){ // In case a descendant class needs to delete more data. 
+
+    }
 }
     // // use the urbanness to determine which building to create
     // var buildingtype = "highrise";
