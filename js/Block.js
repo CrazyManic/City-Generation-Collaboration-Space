527317805fc38500dc775bbded61eb05d7210c8d
JohnCollns <50039786+JohnCollns@users.noreply.github.com>
2022-05-03 10:22:37 +1000

    Added white street lane dividing lines

--- js/Block.js
+++ js/Block.js
@@ -24,23 +24,40 @@ class Block {
         //this.floor.position = position;
         this.roadGeoLong = new THREE.PlaneGeometry(streetWidth,gridSize);
         this.roadGeoShort = new THREE.PlaneGeometry(gridSize - streetWidth, streetWidth);
+        this.stripGeo = new THREE.PlaneGeometry((gridSize - streetWidth) / 2, streetWidth / 6);
         this.roadColour = new THREE.Color(0.1,0.1,0.1);
+        this.stripColour = new THREE.Color(0.9,0.9,0.9);
         this.roadMat = new THREE.MeshLambertMaterial();
         this.roadMat.color = this.roadColour;
+        this.stripMat = new THREE.MeshLambertMaterial();
+        this.stripMat.color = this.stripColour;
         this.roadPlanes = [];
+        this.stripPlanes = [];
         for (var i=0; i<4; i++){
             var roadMesh;
+            var stripMesh;
+            stripMesh = new THREE.Mesh(this.stripGeo,this.stripMat);
+            stripMesh.position.set(this.position.x, this.position.y + 0.01, this.position.z);// raise it just above the road to stop z-fighting
+            this.stripPlanes.push(stripMesh);
             if (i<2){ // long roads along z axis
                 roadMesh = new THREE.Mesh(this.roadGeoLong, this.roadMat);
                 roadMesh.position.set(this.position.x, this.position.y, this.position.z);
                 roadMesh.position.x += (i%2==0?1:-1) * gridSize / 2;
+                stripMesh.position.x += (i%2==0?1:-1) * gridSize / 2;
+                stripMesh.rotation.z += flatRotation;
             } else {  // short roads along x asix
                 roadMesh = new THREE.Mesh(this.roadGeoShort, this.roadMat);
                 roadMesh.position.set(this.position.x, this.position.y, this.position.z);
                 roadMesh.position.z += (i%2==0?1:-1) * gridSize / 2;
+                
+                stripMesh.position.z += (i%2==0?1:-1) * gridSize / 2;
             }
             roadMesh.rotation.x = flatRotation;
             this.roadPlanes.push(roadMesh);
+            for (var j=0; j<this.stripPlanes.length;j++){
+                this.stripPlanes[j].rotation.x = flatRotation;
+                scene.add(this.stripPlanes[j]);
+            }
             scene.add(roadMesh);
         }
     }
@@ -54,6 +71,8 @@ class Block {
         for (var i=3; i >= 0; i--){
             this.scene.remove(this.roadPlanes[i]);
             delete this.roadPlanes[i].object;
+            this.scene.remove(this.stripPlanes[i]);
+            delete this.stripPlanes[i].object;
         }
         delete this;
     }
