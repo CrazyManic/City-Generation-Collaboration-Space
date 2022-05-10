d9a05d885a5ffea4dc6265ded780e7d81d84611e
JohnCollns <50039786+JohnCollns@users.noreply.github.com>
2022-05-10 12:14:28 +1000

    Moved loader to main file. Next change to OBJ
    My PLY files are corrupted, going to use my OBJs instead.

--- js/MeshBuilding.js
+++ js/MeshBuilding.js
@@ -1,40 +1,38 @@
-const directory = "Models/";
-var plyLoader = new THREE.PLYLoader();
 const midGrey = new THREE.Color(0.5,0.5,0.5);
 
 class MeshBuilding extends Building {
     Generate(urbanness, paramColours, mesh_files){
         this.meshes = [];
         //console.log("Generating MeshBuilding");
-        for (var i=0; i < mesh_files.length; i++){
-            var itMesh;
-            console.log("iterating through mesh_files: "+ directory+mesh_files[i]);
-            switch (mesh_files[i].slice(mesh_files[i].length - 3)){ // last three chars of the string
-                case "obj":
-                    //
-                    break;
-                case "ply":
-                    plyLoader.load(directory+mesh_files[i], function(impGeometry){
-                        var impMaterial = new THREE.MeshLambertMaterial();
-                        if (i < paramColours.size)
-                            impMaterial.color = paramColours[i]
-                        else
-                            impMaterial.color = midGrey;
-                        impGeometry.computeVertexNormals();
-                        itMesh = new THREE.Mesh(impGeometry, impMaterial);
-                        //itMesh.position.set(this.position.x, this.position.y, this.position.z);
-                        console.log('loaded a ply file');
-                    });
-                    break;
-            }
-            console.log(itMesh);
-            if (itMesh != null && itMesh != undefined){
-                itMesh.position.set(this.position.x, this.position.y, this.position.z);
-                this.meshes.push(itMesh);
-                scene.add(itMesh);
-                console.log("adding itmesh to scene");
-            }
-        }
+        // for (var i=0; i < mesh_files.length; i++){
+        //     var itMesh;
+        //     console.log("iterating through mesh_files: "+ directory+mesh_files[i]);
+        //     switch (mesh_files[i].slice(mesh_files[i].length - 3)){ // last three chars of the string
+        //         case "obj":
+        //             //
+        //             break;
+        //         case "ply":
+        //             plyLoader.load(directory+mesh_files[i], function(impGeometry){
+        //                 var impMaterial = new THREE.MeshLambertMaterial();
+        //                 if (i < paramColours.size)
+        //                     impMaterial.color = paramColours[i]
+        //                 else
+        //                     impMaterial.color = midGrey;
+        //                 impGeometry.computeVertexNormals();
+        //                 itMesh = new THREE.Mesh(impGeometry, impMaterial);
+        //                 //itMesh.position.set(this.position.x, this.position.y, this.position.z);
+        //                 console.log('loaded a ply file');
+        //             });
+        //             break;
+        //     }
+        //     console.log(itMesh);
+        //     if (itMesh != null && itMesh != undefined){
+        //         itMesh.position.set(this.position.x, this.position.y, this.position.z);
+        //         this.meshes.push(itMesh);
+        //         scene.add(itMesh);
+        //         console.log("adding itmesh to scene");
+        //     }
+        // }
     }
 
     DestroyExtras(){
